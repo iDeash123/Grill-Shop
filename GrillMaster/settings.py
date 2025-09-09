81809bfa6e0d8a7f353891f7730ab806168d2728
iDeash123 <istresnaa@gmail.com>
2025-09-09 19:11:08 +0300

    refactor: move SECRET_KEY to environment variable and update .gitignore

--- GrillMaster/settings.py
+++ GrillMaster/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-stn4u9^po5*2c-!p=_$6=o*#nhx+cymv!pefo7oz50-l9#er&h'
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
